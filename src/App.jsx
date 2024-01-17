6c0de11779989dd7fb26f5f77c4673f2e1e9e150
ayodhyadam <156931035+ayodhyadam@users.noreply.github.com>
2024-01-17 15:51:42 +0000

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
+        
         <p className="small">
           Edit <code>src/App.jsx</code> and save to reload.
         </p>
